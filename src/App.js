eb721a822e4152931c7019d805c4dfadb3b8edff
Hector Vicencio <hector.vicencio.reyes@mail.com>
2019-11-05 14:41:53 -0300

    fixed components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import WeatherLocation from "./components/WeatherLocation";
 
--- src/components/weatherComponent/Location.js
+++ src/components/weatherComponent/Location.js
@@ -1,4 +1,4 @@
-import React , { Component } from "react";
+import React from "react";
 
 const Location = (props) => {
     const { city } = props;
