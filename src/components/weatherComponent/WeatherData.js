f37a54af10edd2039948e9b905bf149c1e3bb38a
Hector Vicencio <hector.vicencio.reyes@gmail.com>
2019-11-19 04:10:25 -0300

    adding some fixes

--- src/components/weatherComponent/WeatherData.js
+++ src/components/weatherComponent/WeatherData.js
@@ -1,12 +1,26 @@
 import React from "react";
 import WeatherTemperature from "./weatherDataComponent/WeatherTemperature";
 import WeatherExtraInfo from "./weatherDataComponent/WeatherExtraInfo";
+import { 
+    CLOUD, 
+    CLOUDY, 
+    SUN, 
+    RAIN, 
+    SNOW, 
+    WINDY 
+} from '../../constants/weathers';
 
 const WeatherData = () => {
     return (
         <div>
-            <WeatherTemperature/>
-            <WeatherExtraInfo humidity={80} wind={"10 m/s"}/>
+            <WeatherTemperature 
+                temperature={20} 
+                weatherState={CLOUDY}
+            />
+            <WeatherExtraInfo 
+                humidity={80} 
+                wind={"10 m/s"}
+            />
         </div>
     )
 }
--- src/components/weatherComponent/weatherDataComponent/WeatherTemperature.js
+++ src/components/weatherComponent/weatherDataComponent/WeatherTemperature.js
@@ -1,7 +1,39 @@
 import React from "react"
+import WeatherIcons from "react-weathericons";
+import { 
+    CLOUD, 
+    CLOUDY, 
+    SUN, 
+    RAIN, 
+    SNOW, 
+    WINDY 
+} from '../../../constants/weathers';
 
-const WeatherTemperature = () => (
-    <div> Temperatura 5°C</div>
-)
+const icons = {
+    [CLOUD]: "cloud",
+    [CLOUDY]: "cloudy",
+    [SUN]: "day-sunny",
+    [RAIN]: "rain",
+    [SNOW]: "snow",
+    [WINDY]: "windy"
+};
+
+const getWeatherState = weatherState => {
+    const icon = icons[weatherState];
+    if (icon)
+        return <WeatherIcons name={icon} size="2x"/>;
+    else 
+        return <WeatherIcons name="day-sunny" size="2x"/>;
+};
+
+const WeatherTemperature = ({ temperature, weatherState}) => (  
+    <div> 
+        {
+            getWeatherState(weatherState)
+        }
+        <span>{` ${temperature} °C`}</span>
+    </div>
+);
+
 
 export default WeatherTemperature;
