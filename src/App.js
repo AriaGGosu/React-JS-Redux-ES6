105ca5ad90df607af0d74707bba3302b2edc7680
Hector Vicencio <hector.vicencio.reyes@gmail.com>
2019-12-06 23:40:34 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import WeatherLocation from "./components/WeatherLocation";
 function App() {
 
   const cities = ['Santiago,cl', 'Viña del mar,cl','Tokyo,jp','Osaka,jp','Yokohama,jp'];
-  const handleWeatherLocationClick = () => {
-    console.log("handleWeatherLocationClick")
+
+  const handleWeatherLocationClick = city => {
+    console.log(city)
   }
   return (
     <div className="App">
       {cities.map(( city , key) => 
-        <WeatherLocation onWeatherLocationClick={ () => handleWeatherLocationClick(city) } city={city} key={key}/>
+        <WeatherLocation 
+          onWeatherLocationClick={ () => handleWeatherLocationClick(city) } 
+          city={city} 
+          key={key}
+        />
       )}
     </div>
   );
